162f74cebf1c42c455a707e26c3c6a103366b6cb
jaimejaime19 <jaime_jaime19@hotmail.com>
2024-04-22 13:29:55 -0700

    Updates

--- src/containers/index.js
+++
@@ -1,5 +0,0 @@
-export {default as Footer} from './footer/Footer';
-export {default as Header} from './header/Header';
-export {default as Whoami} from './whoami/Whoami';
-export {default as Extra} from './extra/Extra';
-export {default as Accomplish} from './accomplish/Accomplish'
