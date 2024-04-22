09cad6e99b022e8f48ea7a96972746860d9d9c0d
jaimejaime19 <jaime_jaime19@hotmail.com>
2024-04-22 14:23:59 -0700

    Header text

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     <div className='my__header section__padding' id="home">
         <div className='my__header-content'>
           <h1 className='gradient__text'>Welcome to the Website of Jaime!</h1>
-          <p> This website was made using ReactJS. Extensive use of HTML, CSS, and Javascript makes this site look excellent (and mobile responsive)!</p>
+          <p> This website was made using ReactJS. Extensive use of HTML, CSS, and Javascript makes this site look excellent and mobile responsive! This website is hosted on Github Pages.</p>
           <div className='my__header-content__input'>
             <input type='email' placeholder='Your Email Here (WIP)' title='Email functionality WIP'></input>
             <button type='button' title='Email functionality WIP'>Get Started!</button>
